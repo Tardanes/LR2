c43e4c7da2268d90bc0b7c98dfc67d3722f09f88
Tardanes <tardanes@vivaldi.net>
2023-09-14 20:51:38 +0300

    Success commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        short s1 = 99, s2 = 99;
+        int d1, d2;
+        float f1, f2;
+        byte b = -10;
+        boolean q = true;
+        s1 = (short)Math.pow(b, 2);
+        s2 = (short)(Math.random()*(10-0)+0);
+        Random random = new Random(69);
+        d2 = (random.nextInt()*(9999-1)+1);
+        d1 = (s2/(s1*s2));
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(s1);
+        System.out.println(s2);
+        Scanner in = new Scanner(System.in);
+        f1 = in.nextFloat();
+        f2 = in.nextFloat();
+        double y = (1/f1)-(2/f2)+(Math.sin(s1)/Math.log10(s2));
+        System.out.println(y);
     }
 }
